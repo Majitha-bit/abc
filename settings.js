8245d8c25a1f64b0927600d717dff1a2646d8666
R.M.Majitha Sanmika Rathnayaka <majithasanmika94@gmail.com>
2026-01-17 07:22:44 +0530

    Update.yml

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=26a7be9240292adc','𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=c37646e923efc1f7': process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=26a7be9240292adc': process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
